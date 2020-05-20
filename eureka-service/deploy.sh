3a350d35907a097c1d68ef3a7fbcb9fc629019c4
youlingman <chenyoucyl@gmail.com>
2020-05-20 19:12:37 +0800

    simple HA docker deploy for eureka server

--- eureka-service/deploy.sh
+++ eureka-service/deploy.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
-docker run -d -p 9100:8761 --name eureka-service1 cyl/eureka-service
-docker run -d -p 9101:8761 --name eureka-service2 cyl/eureka-service
-docker run -d -p 9102:8761 --name eureka-service3 cyl/eureka-service
+COMMAND=$1
+SERVICES=("eureka-service1" "eureka-service2" "eureka-service3")
+
+echo "$COMMAND"
+# start all eureka-service container
+if [ "$COMMAND" = "start" ]; then
+    for s in ${SERVICES[*]}; do
+        docker start `docker ps -a | grep "$s" | awk '{print \$1}'`
+    done
+fi
+
+# stop all eureka-service container
+if [ "$COMMAND" = "stop" ]; then
+    for s in ${SERVICES[*]}; do
+        docker stop `docker ps -a | grep "$s" | awk '{print \$1}'`
+    done
+fi
+
+# rm all eureka-service container
+if [ "$COMMAND" = "rm" ]; then
+    for s in ${SERVICES[*]}; do
+        docker rm `docker ps -a | grep "$s" | awk '{print \$1}'`
+    done
+fi
+
+# init network and container
+if [ "$COMMAND" = "init" ]; then
+    NETWORK=`docker network ls | grep eureka-service | awk '{print \$1}'`
+    if [ -z "$NETWORK" ]; then
+        docker network create --driver bridge eureka-service
+    fi
+    for i in $(seq 0 ${#SERVICES[@]}); do
+        if [ ! "" = "${SERVICES[i]}" ]; then
+            docker run -d -p $(expr 9100 + $i):8761 --name "${SERVICES[i]}" --network=eureka-service --hostname "${SERVICES[i]}" cyl/eureka-service --spring.profiles.active="${SERVICES[i]}"
+        fi
+    done
+fi
