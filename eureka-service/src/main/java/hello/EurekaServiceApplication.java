df36a03bb40a06d17ca7e29891c8a12638d3cbe7
youlingman <chenyoucyl@gmail.com>
2020-05-28 17:41:11 +0800

    add config for producer

--- eureka-service/src/main/java/hello/EurekaServiceApplication.java
+++ eureka-service/src/main/java/hello/EurekaServiceApplication.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@RefreshScope // seems not work to add refresh scope here
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
--- producer/src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
+++ producer/src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class ServiceRegistrationAndDiscoveryClientApplication {
     }
 }
 
+@RefreshScope
 @RestController
 class ProducerRestController {
 
