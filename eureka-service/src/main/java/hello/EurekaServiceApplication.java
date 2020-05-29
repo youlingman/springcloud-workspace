6af06e35fa7ab8c1065b02ee5436710b2e6aa927
youlingman <chenyoucyl@gmail.com>
2020-05-29 18:49:16 +0800

    eureka server

--- eureka-service/src/main/java/hello/EurekaServiceApplication.java
+++ eureka-service/src/main/java/hello/EurekaServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@RefreshScope // seems not work to add refresh scope here
+//@RefreshScope // seems not work to add refresh scope here
+// update: no need to add RefreshScope here, onApplicationEvent in EurekaServerAutoConfiguration already do this
+// only listen eureka.client.region / eureka.client.service-url.* / eureka.client.availability-zones.* change event to refresh peer list
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
